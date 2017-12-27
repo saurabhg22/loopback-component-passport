97ba438399759df2853144fafbc8adf946785ab2
Saurabh Gupta <saurabh@Mithyas-Air.Dlink>
2017-12-27 15:09:33 +0530

    Added support for custom users models for third party logins.

--- test/init.js
+++ test/init.js
@@ -1,19 +1,17 @@
-// Copyright IBM Corp. 2014,2016. All Rights Reserved.
-// Node module: loopback-component-passport
-// This file is licensed under the Artistic License 2.0.
-// License text available at https://opensource.org/licenses/Artistic-2.0
-
-'use strict';
 var loopback = require('loopback');
-var passport = module.exports = require('../lib/index');
+module.exports = require('../lib/index');
 
-var db = loopback.createDataSource('db', {connector: 'memory'});
+// setup default data sources
+loopback.setDefaultDataSourceForType('db', {
+  connector: loopback.Memory
+});
 
-loopback.Application.attachTo(db);
-loopback.User.attachTo(db);
-loopback.AccessToken.attachTo(db);
+loopback.setDefaultDataSourceForType('mail', {
+  connector: loopback.Mail,
+  transports: [
+    {type: 'STUB'}
+  ]
+});
 
-passport.UserIdentity.attachTo(db);
-passport.UserCredential.attachTo(db);
-passport.ApplicationCredential.attachTo(db);
-
+// auto attach data sources to models
+loopback.autoAttach();
